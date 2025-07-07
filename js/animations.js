6144b78f8cb337daee840efb44776b195301c08b
yusif641 <gorc141408@gmail.com>
2025-07-07 12:44:34 +0300

    animations

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger, SplitText)
-});
+function checkForVisibility() {
+    var headers = document.querySelectorAll(".split");
+    headers.forEach(function (header) {
+        if (isElementInViewport(header)) {
+            header.classList.add("splitVisible");
+        } else {
+            header.classList.remove("splitVisible");
+        }
+    });
+}
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+if (window.addEventListener) {
+    addEventListener("DOMContentLoaded", checkForVisibility, false);
+    addEventListener("load", checkForVisibility, false);
+    addEventListener("scroll", checkForVisibility, false);
+}
